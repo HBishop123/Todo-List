89b9947a57efe8ba7eba006f26930df514cdbb61
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-09 16:08:14 +0000

    bug fixes

--- src/DOM_elements.js
+++ src/DOM_elements.js
@@ -11,11 +11,20 @@ const createTaskDisplay = {
     const addTask = document.getElementById("add-task");
     const form = document.querySelector("#addTaskForm");
     const hideTaskDisplayButton = document.querySelector("#cancel");
+    const hideTaskDisplayButtonWithSubmit = document.querySelector("#submit");
+
+    hideTaskDisplayButtonWithSubmit.addEventListener("click", () => {
+      form.style.display = "none";
+      addTask.style.display = "block";
+    });
+
     hideTaskDisplayButton.addEventListener("click", () => {
       form.style.display = "none";
       addTask.style.display = "block";
     });
   },
 };
+
+
 
 export default createTaskDisplay;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
+let arr = [];
+console.log(arr);
 class toDo {
-  constructor(title, description, dueDate, priority, notes) {
+  constructor(title, dueDate, priority, notes, description) {
     this.title = title;
-    this.descritpion = description;
+    this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.notes = notes;
   }
 }
 
-import createTaskDisplay from "./DOM_elements.js"
+const useFormData = {
+  grabFormData: function (e) {
+    e.preventDefault();
 
-createTaskDisplay.appendDisplay()
-createTaskDisplay.hideDisplay()
+    const task = {
+      title: document.getElementById("form-text").value,
+      date: document.getElementById("date").value,
+      priority: document.querySelector('input[name="priority"]:checked').value,
+    };
+    const title = task.title;
+    const dueDate = task.date;
+    const priority = task.priority;
+
+    let result = new toDo(title, dueDate, priority);
+    arr.push(result);
+    document.forms[0].reset();
+    return this.grabFormData;
+  },
+  attachEventListener: function () {
+    document.addEventListener("DOMContentLoaded", () => {
+      document
+        .getElementById("submit")
+        .addEventListener("click", this.grabFormData);
+    });
+  },
+};
+useFormData.attachEventListener()
+
+
+
+
+
+
+
+
+
+import createTaskDisplay from "./DOM_elements.js";
+
+createTaskDisplay.appendDisplay();
+createTaskDisplay.hideDisplay();
