b9b020d771720f027e27ef0234a7805b5af4f48e
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-09 11:39:28 +0000

    changed index file to add class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+class toDo {
+    constructor(title, description, dueDate, priority, notes){
+        this.title = title
+        this.descritpion = description
+        this.dueDate = dueDate
+        this.priority = priority
+        this.notes = notes
+    }
+}
 
