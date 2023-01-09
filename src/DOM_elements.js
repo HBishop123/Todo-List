499ab12d5a4f88783ec9035517ba77597f6b25ad
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-09 13:11:25 +0000

    added object to both close and open the form, as well as imported it to the index.js file

--- src/DOM_elements.js
+++ src/DOM_elements.js
@@ -1,9 +1,18 @@
-createAddTaskDisplay = {
-   
-    AppendDisplay: function(){
-        const addTask = document.getElementById('add-task')
-        addTask.addEventListener('click', () => {
+const createTaskDisplay = {
+  appendDisplay: function () {
+    const addTask = document.getElementById("add-task");
+    addTask.addEventListener("click", () => {
+      const form = document.querySelector("#addTaskForm");
+      form.style.display = "block";
+    });
+  },
+  hideDisplay: function () {
+    const hideTaskDisplayButton = document.querySelector("#cancel");
+    hideTaskDisplayButton.addEventListener("click", () => {
+      const form = document.querySelector("#addTaskForm");
+      form.style.display = "none";
+    });
+  },
+};
 
-        })
-    }
-}
+export default createTaskDisplay;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 class toDo {
-    constructor(title, description, dueDate, priority, notes){
-        this.title = title
-        this.descritpion = description
-        this.dueDate = dueDate
-        this.priority = priority
-        this.notes = notes
-    }
+  constructor(title, description, dueDate, priority, notes) {
+    this.title = title;
+    this.descritpion = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+  }
 }
 
+import createTaskDisplay from "./DOM_elements.js"
+
+createTaskDisplay.appendDisplay()
+createTaskDisplay.hideDisplay()
